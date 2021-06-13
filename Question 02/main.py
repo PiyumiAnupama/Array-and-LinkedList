3953a5eaac0b3a155d5a23fc39095a0197bd7be5
PiyumiAnupama <96477@fhss.sjp.ac.lk>
2021-06-13 14:31:35 +0530

    Modify push, pop, top functions and Constructor

--- Question 02/main.py
+++ Question 02/main.py
@@ -1,18 +1,39 @@
 from Stack import Stack
 
 myStack=Stack(6)
+print("Number of Stack's Nodes : ",myStack.size())
+
+print(" ")
+
+print(myStack.top())
+print(myStack.pop())
+
+print(" ")
+
 print(myStack.isFullStack())
 print(myStack.isEmptyStack())
-print(myStack.pop())
+
+print(" ")
+
 myStack.push(5)
 myStack.push(10)
 myStack.push(15)
 myStack.push(20)
 myStack.push(25)
+print("Remove Node is : ",myStack.pop())
+print("Remove Node is : ",myStack.pop())
+
+print(" ")
+
+print("Top Node is : ",myStack.top())
+print("Number of Stack's Nodes : ",myStack.size())
 myStack.push(30)
 myStack.push(35)
-print(myStack.pop())
-print(myStack.size())
+myStack.push(40)
+myStack.push(45)
+print("Number of Stack's Nodes : ",myStack.size())
+
+print(" ")
+
 print(myStack.isFullStack())
 print(myStack.isEmptyStack())
-
